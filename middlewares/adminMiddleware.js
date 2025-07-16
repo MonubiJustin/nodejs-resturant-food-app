0df6b39f8e56d47562017d0c7f1b36b40d9d86d6
Monubi Justin <monubijustin@gmail.com>
2025-07-17 00:06:39 +0300

    Update adminMiddleware.js
    I have removed the else statement since we are directly returning a res ponse at the if block. This makes the code look more cleaner and less noisy

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -3,14 +3,9 @@ const userModel = require("../models/userModel");
 module.exports = async (req, res, next) => {
   try {
     const user = await userModel.findById(req.body.id);
-    if (user.usertype !== "admin") {
-      return res.status(401).send({
-        success: false,
-        message: "Only Admin ACess ",
-      });
-    } else {
-      next();
-    }
+    if (user.usertype !== "admin") return res.status(401).send({ success: false, message: "Only Admin Access"});
+    
+    next();  
   } catch (error) {
     console.log(error);
     res.status(500).send({
