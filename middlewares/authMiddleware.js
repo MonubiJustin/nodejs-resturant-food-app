65fdb45eba013f4150068946a16831c3725957a3
Monubi Justin <monubijustin@gmail.com>
2025-07-17 23:52:13 +0300

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,26 +1,18 @@
 const JWT = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
-  try {
-    // get token
-    const token = req.headers["authorization"].split(" ")[1];
-    JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
-      if (err) {
-        return res.status(401).send({
-          success: false,
-          message: "Un-Authorize User",
-        });
-      } else {
-        req.body.id = decode.id;
-        next();
-      }
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      success: false,
-      message: "Please provide Auth Token",
-      error,
-    });
+  const authHeader = req.header("Authorization");
+  if (!authHeader || !authHeader.startsWith("Bearer ")) return res.status(401).json({msg: "Access Denied. No auth token provided")
+
+  const token = authHeader.split(' ')[1];
+  if (!token) return res.status(401).json({msg: "Access Denied. No auth token provided"});
+
+  try{
+    const decoded = JWT.verify(token, process.env.JWY_SECRET);
+    req.body.id = decode.id;
+    next()
+  }catch(err){
+    console.log(err.stack)
+    res.status(400).json({msg: "Invalid Token"});
   }
-};
+}
